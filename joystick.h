f620649a6b6cebb240d86ea77293db7ccce2146e
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2020-06-18 21:46:21 +0100

    Move bit positions into JoystickResult

--- joystick.h
+++ joystick.h
@@ -33,6 +33,42 @@ public:
 		// Nothing here
 	}
 
+	void set_fire_pressed() {
+		data |= (1 << SHIFT_A);
+	}
+
+	void set_a_pressed() {
+		data |= (1 << SHIFT_A);
+	}
+
+	void set_b_pressed() {
+		data |= (1 << SHIFT_B);
+	}
+
+	void set_c_pressed() {
+		data |= (1 << SHIFT_C);
+	}
+
+	void set_up_pressed() {
+		data |= (1 << SHIFT_UP);
+	}
+
+	void set_down_pressed() {
+		data |= (1 << SHIFT_DOWN);
+	}
+
+	void set_left_pressed() {
+		data |= (1 << SHIFT_LEFT);
+	}
+
+	void set_right_pressed() {
+		data |= (1 << SHIFT_RIGHT);
+	}
+
+	void set_start_pressed() {
+		data |= (1 << SHIFT_START);
+	}
+
 	bool is_fire_pressed() const {
 		return (data & (1 << SHIFT_A)) != 0;
 	}
@@ -69,9 +105,29 @@ public:
 		return (data & (1 << SHIFT_START)) != 0;
 	}
 
+	bool is_left_right_pressed() const {
+		return is_left_pressed() && is_right_pressed();
+	}
+
+	void clear_left_right_pressed() {
+		data &= ~(1 << SHIFT_LEFT);
+		data &= ~(1 << SHIFT_RIGHT);
+	}
+
 	uint16_t value() {
 		return data;
 	}
+
+	bool operator==(const JoystickResult& rhs) const {
+		return data == rhs.data;
+	}
+
+	bool operator!=(const JoystickResult& rhs) const {
+		return data != rhs.data;
+	}
+
+private:
+	uint16_t data;
 
 	static constexpr uint8_t SHIFT_UP = 0;
 	static constexpr uint8_t SHIFT_DOWN = 1;
@@ -82,9 +138,6 @@ public:
 	static constexpr uint8_t SHIFT_C = 6;
 	static constexpr uint8_t SHIFT_START = 7;
 	static constexpr uint8_t MASK_LEFT_RIGHT = ( 1 << SHIFT_LEFT ) | ( 1 << SHIFT_RIGHT );
-
-private:
-	uint16_t data;
 };
 
 /**
@@ -140,45 +193,45 @@ public:
 	/// @return true if value different to the last call to `read`, otherwise false
 	///
 	bool scan() {
-		uint16_t new_pinmap = 0;
+		JoystickResult new_pinmap;
 		// Pins are active low
 		if (digitalRead(m_pin_up) == 0) {
-			new_pinmap |=  1 << JoystickResult::SHIFT_UP;
+			new_pinmap.set_up_pressed();
 		};
 		if (digitalRead(m_pin_down) == 0) {
-			new_pinmap |=  1 << JoystickResult::SHIFT_DOWN;
+			new_pinmap.set_down_pressed();
 		};
 		if (digitalRead(m_pin_a_b) == 0) {
-			new_pinmap |=  1 << JoystickResult::SHIFT_A;
+			new_pinmap.set_a_pressed();
 		};
 		if (digitalRead(m_pin_start_c) == 0) {
-			new_pinmap |=  1 << JoystickResult::SHIFT_START;
+			new_pinmap.set_start_pressed();
 		};
 		if(digitalRead(m_pin_gnd_left) == 0) {
-			new_pinmap |= 1 << JoystickResult::SHIFT_LEFT;
+			new_pinmap.set_left_pressed();
 		};
 		if(digitalRead(m_pin_gnd_right) == 0) {
-			new_pinmap |= 1 << JoystickResult::SHIFT_RIGHT;
+			new_pinmap.set_right_pressed();
 		};
-		if ( ( new_pinmap & JoystickResult::MASK_LEFT_RIGHT ) == JoystickResult::MASK_LEFT_RIGHT )
+		if ( new_pinmap.is_left_right_pressed() )
 		{
 			// Impossible for left and right to be active at the same time, so
 			// we must have a SEGA MegaDrive pad.
 			digitalWrite(m_pin_select, 1);
 			// Clear the left/right pins as they aren't actually set
-			new_pinmap &= ~JoystickResult::MASK_LEFT_RIGHT;
+			new_pinmap.clear_left_right_pressed();
 			// Read the alternative pins
 			if (digitalRead(m_pin_gnd_left) == 0) {
-				new_pinmap |= 1 << JoystickResult::SHIFT_LEFT;
+				new_pinmap.set_left_pressed();
 			}
 			if (digitalRead(m_pin_gnd_right) == 0) {
-				new_pinmap |= 1 << JoystickResult::SHIFT_RIGHT;
+				new_pinmap.set_right_pressed();
 			}
 			if (digitalRead(m_pin_a_b) == 0) {
-				new_pinmap |= 1 << JoystickResult::SHIFT_B;
+				new_pinmap.set_b_pressed();
 			}
 			if (digitalRead(m_pin_start_c) == 0) {
-				new_pinmap |= 1 << JoystickResult::SHIFT_C;
+				new_pinmap.set_c_pressed();
 			}
 			// Turn select off again
 			digitalWrite(m_pin_select, 0);
@@ -188,8 +241,8 @@ public:
 	}
 
 private:
-	uint16_t m_pinmap;
-	uint16_t m_pinmap_old;
+	JoystickResult m_pinmap;
+	JoystickResult m_pinmap_old;
 
 	const int m_pin_start_c;
 	const int m_pin_a_b;
