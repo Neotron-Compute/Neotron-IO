0bed6f034b2bc15862e7b122c6461f37c0391647
Jonathan Pallant <github@thejpster.org.uk>
2020-08-31 23:32:48 +0100

    Testing the HID impl.

--- tests/runner.cpp
+++ tests/runner.cpp
@@ -4,65 +4,65 @@
 
 #define TEST_MODE_NO_PRIVATE
 
-#include <cstdio>
 #include <cstdint>
+#include <cstdio>
 #include <ctime>
 
 #include "arduino_stubs.h"
 
+#include "hid.h"
 #include "ps2.h"
 
 #define MAX_PINS 4
 int pin_results[MAX_PINS];
 
-typedef bool (*test_fn_t)(const char **sz_name);
+typedef bool ( *test_fn_t )( const char** sz_name );
 
-#define DEFINE_TEST(test_name)                  \
-	static bool test_name##_inner(void);        \
-	static bool test_name(const char **sz_name) \
-	{                                           \
-		if (sz_name)                            \
-		{                                       \
-			*sz_name = __func__;                \
-		}                                       \
-		return test_name##_inner();             \
-	}                                           \
-	static bool test_name##_inner(void)
+#define DEFINE_TEST( test_name )                  \
+	static bool test_name##_inner( void );        \
+	static bool test_name( const char** sz_name ) \
+	{                                             \
+		if ( sz_name )                            \
+		{                                         \
+			*sz_name = __func__;                  \
+		}                                         \
+		return test_name##_inner();               \
+	}                                             \
+	static bool test_name##_inner( void )
 
 // Check PS/2 object times out
-DEFINE_TEST(ps2_timeout)
+DEFINE_TEST( ps2_timeout )
 {
-	Ps2 ps2(0, 1);
+	Ps2 ps2( 0, 1 );
 	ps2.m_state = Ps2State::ReadingWord;
 	unsigned long target;
 	// Get a value that doesn't wrap
 	do
 	{
 		target = micros() + 1000;
-	} while (target < micros());
+	} while ( target < micros() );
 
 	do
 	{
 		ps2.poll();
-	}
-	while(micros() < target);
-	return (ps2.m_state == Ps2State::Idle);
+	} while ( micros() < target );
+	return ( ps2.m_state == Ps2State::Idle );
 }
 
 // Check PS/2 can collect bits
-DEFINE_TEST(ps2_collect_bits)
+DEFINE_TEST( ps2_collect_bits )
 {
 	// 0 is clk, 1 = data
-	Ps2 ps2(0, 1);
-	uint16_t test_word = (0x03 << 9) | (0xAA << 1);
-	for (int i = 0; i < 11; i++)
+	Ps2 ps2( 0, 1 );
+	uint16_t test_word = ( 0x03 << 9 ) | ( 0xAA << 1 );
+	for ( int i = 0; i < 11; i++ )
 	{
 		pin_results[0] = 1;
 		ps2.poll();
 		ps2.poll();
 		ps2.poll();
 		ps2.poll();
-		pin_results[1] = (test_word & (1 << i)) ? 1 : 0;
+		pin_results[1] = ( test_word & ( 1 << i ) ) ? 1 : 0;
 		pin_results[0] = 0;
 		ps2.poll();
 		ps2.poll();
@@ -72,85 +72,141 @@ DEFINE_TEST(ps2_collect_bits)
 
 	int read_byte = ps2.readBuffer();
 	// Should have collected 0xAA
-	return (read_byte == 0xAA);
+	return ( read_byte == 0xAA );
 }
 
-DEFINE_TEST(ps2_validate_words)
+DEFINE_TEST( ps2_validate_words )
 {
-	const int inputs[] = {0x600, 0x606, 0x402};
-	const int outputs[] = {0x00, 0x03, 0x01};
+	const int inputs[] = { 0x600, 0x606, 0x402 };
+	const int outputs[] = { 0x00, 0x03, 0x01 };
 	bool pass = true;
-	for (int i = 0; i < 3; i++)
+	for ( int i = 0; i < 3; i++ )
 	{
-		int result = Ps2::validateWord(inputs[i]);
-		pass &= (result == outputs[i]);
+		int result = Ps2::validateWord( inputs[i] );
+		pass &= ( result == outputs[i] );
 	}
 	return pass;
 }
 
-DEFINE_TEST(ps2_encode_bytes)
+DEFINE_TEST( ps2_encode_bytes )
 {
 	bool pass = true;
-	for(int i = 0; i < 256; i++)
+	for ( int i = 0; i < 256; i++ )
 	{
-		uint16_t word = Ps2::encodeByte((uint8_t) i);
-		uint8_t output = Ps2::validateWord(word);
-		if (i != output) {
-			printf("%02x != %02x (%04x)\n", i, output, word);
+		uint16_t word = Ps2::encodeByte( (uint8_t)i );
+		uint8_t output = Ps2::validateWord( word );
+		if ( i != output )
+		{
+			printf( "%02x != %02x (%04x)\n", i, output, word );
 			pass = false;
 		}
 	}
 	return pass;
 }
 
-int bitRead(int word, int bit)
+DEFINE_TEST( hid_encode_main_descriptor )
 {
-	return ((word & (1 << bit)) != 0) ? 1 : 0;
+	uint8_t buffer[64] = { 0 };
+	HidDescriptor d = HidDescriptor(
+	    /* wReportDescLength */ 3,
+	    /* wReportDescRegister */ 4,
+	    /* wInputRegister */ 5,
+	    /* wMaxInputLength */ 6,
+	    /* wOutputRegister */ 7,
+	    /* wMaxOutputLength */ 8,
+	    /* wCommandRegister */ 9,
+	    /* wDataRegister */ 65534,
+	    /* wVendorID */ 11,
+	    /* wProductID */ 12,
+	    /* wVersionID */ 13 );
+	const uint8_t example[30] = {
+	    30, 0, 0, 1,    3,    0,  4, 0,  5, 0,  6, 0, 7, 0, 8,
+	    0,  9, 0, 0xFE, 0xFF, 11, 0, 12, 0, 13, 0, 0, 0, 0, 0,
+	};
+
+	size_t result = d.encode_into_buffer( buffer, sizeof( buffer ) );
+	bool pass = ( result == 30 ) &&
+	            ( memcmp( buffer, example, sizeof( example ) ) == 0 );
+	if ( !pass )
+	{
+		for ( size_t i = 0; i < sizeof( example ); i++ )
+		{
+			printf( "%02x = %02x? %c\r\n", buffer[i], example[i],
+			        buffer[i] == example[i] ? 'Y' : 'N' );
+		}
+		printf( "result = %zu\r\n", result );
+	}
+	return pass;
 }
 
-void digitalWrite(int pin, bool level)
+DEFINE_TEST( hid_encode_command )
+{
+	uint8_t buffer[4] = { 0 };
+	HidCommand d = HidCommand( HidCommandOpcode::OPCODE_GET_REPORT,
+	                           HidCommandReportType::REPORT_TYPE_INPUT, 6 );
+	const uint8_t example[2] = { 2, ( 1 << 4 ) + 6 };
+	size_t result = d.encode_into_buffer( buffer, sizeof( buffer ) );
+	bool pass = ( result == 2 ) &&
+	            ( memcmp( buffer, example, sizeof( example ) ) == 0 );
+	if ( !pass )
+	{
+		for ( size_t i = 0; i < sizeof( example ); i++ )
+		{
+			printf( "0x%02x = 0x%02x? %c\r\n", buffer[i], example[i],
+			        buffer[i] == example[i] ? 'Y' : 'N' );
+		}
+		printf( "result = %zu\r\n", result );
+	}
+	return pass;
+}
+
+int bitRead( int word, int bit )
+{
+	return ( ( word & ( 1 << bit ) ) != 0 ) ? 1 : 0;
+}
+
+void digitalWrite( int pin, bool level )
 {
 #ifdef VERBOSE_DEBUG
-	printf("Writing pin %d = %d\r\n", pin, level);
+	printf( "Writing pin %d = %d\r\n", pin, level );
 #endif
 }
 
-void pinMode(int pin, int mode)
+void pinMode( int pin, int mode )
 {
 #ifdef VERBOSE_DEBUG
-	printf("Setting pin %d mode = %d\r\n", pin, mode);
+	printf( "Setting pin %d mode = %d\r\n", pin, mode );
 #endif
 }
 
-int digitalRead(int pin)
+int digitalRead( int pin )
 {
 #ifdef VERBOSE_DEBUG
-	printf("Reading pin %d = %d\r\n", pin, pin_results[pin]);
+	printf( "Reading pin %d = %d\r\n", pin, pin_results[pin] );
 #endif
 	return pin_results[pin];
 }
 
-unsigned long micros() {
+unsigned long micros()
+{
 	struct timespec now;
-	clock_gettime(CLOCK_MONOTONIC, &now);
+	clock_gettime( CLOCK_MONOTONIC, &now );
 	unsigned long time = now.tv_sec * 1000000;
 	time += now.tv_nsec / 1000;
 	return time;
 }
 
 const test_fn_t TESTS[] = {
-	ps2_timeout,
-	ps2_collect_bits,
-	ps2_validate_words,
-	ps2_encode_bytes,
+    ps2_timeout,      ps2_collect_bits,           ps2_validate_words,
+    ps2_encode_bytes, hid_encode_main_descriptor, hid_encode_command,
 };
 
-int main(int argc, char **argv)
+int main( int argc, char** argv )
 {
-	for (size_t i = 0; i < sizeof(TESTS) / sizeof(TESTS[0]); i++)
+	for ( size_t i = 0; i < sizeof( TESTS ) / sizeof( TESTS[0] ); i++ )
 	{
-		const char *sz_name = NULL;
-		bool result = TESTS[i](&sz_name);
-		printf("Test %-20s: %s\r\n", sz_name, result ? "PASS" : "FAIL");
+		const char* sz_name = NULL;
+		bool result = TESTS[i]( &sz_name );
+		printf( "Test %-20s: %s\r\n", sz_name, result ? "PASS" : "FAIL" );
 	}
 }
